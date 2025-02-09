08a8c3d89ceda85881bc7eebf51ac0511e2d269b
Bridget Ma <bridgetma@college.harvard.edu>
2025-02-09 03:26:25 -0500

    added protocol for handling chat history, send, and delete message requests

--- server/protocols/custom_protocol.py
+++ server/protocols/custom_protocol.py
@@ -63,6 +63,72 @@ def handle_get_conversations(recipient):
         response += f" {user} {unread}"
     return response
 
+def handle_get_chat_history(args):
+    """
+    Handle a client's request for chat history.
+
+    Parameters: [client, user2]
+
+    Returns a response in the format:
+        `1.0 MSGS [1 if user who sent the ealiest message is same as the user receiving this history else 0] 
+        [num msgs] [msg ID (if 1), num words, msg1] [msg ID (if 1), num words, msg2] ...`
+    """
+    client = args[0]
+    user2 = args[1]
+    history = database.get_recent_messages(client, user2)
+    if not history:
+        return "1.0 MSGS "
+    response = "1.0 MSGS " + ("1" if history[0]["sender"] == client else "0")
+    num_messages = 0
+    cur_sender = history[0]["sender"]
+    formatted_messages = ""
+    for message in history:
+        if cur_sender != message["sender"]:
+            formatted_messages = f" {num_messages} {formatted_messages}"
+            response += formatted_messages
+            num_messages = 0
+            formatted_messages = ""
+        num_messages += 1
+        message_text = message["message"]
+        id = message["id"] if message["sender"] == client else -1
+        num_words = message_text.count(" ")+1
+        cur_msg = f" {id} {num_words} {message_text}"
+        formatted_messages += cur_msg
+    formatted_messages = f" {num_messages}{formatted_messages}"
+    response += formatted_messages
+    return response
+
+def handle_send_message(args):
+    """
+    Handle a client's request to send a message.
+
+    Parameters: [sender, recipient, message]
+
+    Returns a response in the format:
+        `1.0 ACK [msg ID]`
+    """
+    sender = args[0]
+    recipient = args[1]
+    message = args[2]
+    msg_id = database.store_message(sender, recipient, message)
+    return f"1.0 ACK {msg_id}"
+
+def handle_delete_messages(args):
+    """
+    Handle a client's request to send a message.
+
+    Parameters: [msg ID]
+
+    Returns a response in the format:
+        `1.0 DEL_MSG [msg ID]`
+    """
+    id = int(args[0])
+    success, errno = database.delete_message(id)
+    if success:
+        return f"1.0 DEL_MSG {id}"
+    else:
+        return f"1.0 ERROR {errno}"
+
 def process_message(message):
     """
     Process a message string according to our custom protocol.
@@ -76,5 +142,11 @@ def process_message(message):
         return handle_create(args)
     elif command == "LOGIN":
         return handle_login(args)
+    elif command == "READ":
+        return handle_get_chat_history(args)
+    elif command == "SEND":
+        return handle_send_message(args)
+    elif command == "DEL_MSG":
+        return handle_delete_messages(args)
     else:
         return f"1.0 ERROR {UNKNOWN_COMMAND}"
